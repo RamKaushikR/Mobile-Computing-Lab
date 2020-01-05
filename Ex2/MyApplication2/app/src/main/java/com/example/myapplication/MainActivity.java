6e02a7d1d0ffe7644f79ab50c52984f9f707393c
RamKaushikR <ramkaushik17125@cse.ssn.edu.in>
2020-01-05 16:30:44 +0530

    Ex2

--- Ex2/MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Ex2/MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,21 +1,28 @@
 package com.example.myapplication;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
+
+import java.io.Serializable;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
         Button button = (Button) findViewById(R.id.submitButton);
         button.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onClick(View v) {
                 /*
@@ -49,7 +57,10 @@ public class MainActivity extends AppCompatActivity {
                 details.age = e3.getText().toString();
 
                 DatePicker d = (DatePicker) findViewById((R.id.simpleDatePicker));
-                details.dob = d;
+                Integer day = (Integer) d.getDayOfMonth();
+                Integer month = (Integer) (d.getMonth() + 1);
+                Integer year = (Integer) d.getYear();
+                details.dob = day.toString() + "/" + month.toString() + "/" + year.toString();
 
                 RadioGroup r = (RadioGroup) findViewById(R.id.RadioGroup);
                 Integer rid = (Integer)r.getCheckedRadioButtonId();
@@ -58,26 +69,55 @@ public class MainActivity extends AppCompatActivity {
                 details.gender = r1.getText().toString();
 
                 final Spinner s = (Spinner) findViewById(R.id.spinner);
-                s.setOnItemSelectedListener(new OnItemSelectedListener() {
-                    @Override
-                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                        String text = s.getSelectedItem().toString();
-                        details.marital = text;
+                String text = s.getSelectedItem().toString();
+                details.marital = text;
 
+                EditText e4 = (EditText) findViewById((R.id.editText6));
+                details.phone = e4.getText().toString();
 
-                    }
+                TimePicker t = (TimePicker) findViewById(R.id.timePicker);
+                Integer hour = (Integer) t.getHour();
+                Integer min = (Integer) t.getMinute();
+                details.time = hour.toString() + ":" + min.toString();
 
-                    @Override
-                    public void onNothingSelected(AdapterView<?> parent) {
-                        details.marital = "Null";
+                CheckBox c1 = (CheckBox) findViewById(R.id.simpleCheckBox1);
+                int flag = 0;
+                String t1 = "";
+                if(c1.isChecked())
+                    t1 = c1.getText().toString();
+                else
+                    flag++;
 
-                    }
-                });
+                CheckBox c2 = (CheckBox) findViewById(R.id.simpleCheckBox2);
+                String t2 = "";
+                if(c2.isChecked())
+                    t2 = c2.getText().toString();
+                else
+                    flag++;
+
+                if(flag != 2)
+                    details.addiction = t1 + " " + t2;
+                else
+                    details.addiction = "None";
+
+                /*
+                Context context = getApplicationContext();
+                Toast toast = Toast.makeText(context, (CharSequence) details.time, Toast.LENGTH_LONG);
+                toast.show();*/
 
                 Context context = getApplicationContext();
-                Toast toast = Toast.makeText(context, details.marital, Toast.LENGTH_LONG);
-                toast.show();
+                Intent intent = new Intent(context, Details.class);
+                intent.putExtra("Details", details);
+                startActivity(intent);
+
+
+
             }
         });
+
+    }
+
+    private void createTable() {
+
     }
 }
